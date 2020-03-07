4c9690df0134a7ca2db0afa930e7c6e8fd6a62db
Shlomi Kushchi <shlomik@gmail.com>
2020-03-07 16:14:15 +0200

    requirements

--- setup.py
+++ setup.py
@@ -10,19 +10,18 @@ with open('README.rst') as readme_file:
 
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
-with open(os.path.join(os.getcwd(), "requirements.txt")) as req:
-    requirements = req.readlines()
 
-setup_requirements = ["certifi==2019.11.28",
-                      "chardet==3.0.4",
-                      "idna==2.9",
-                      "lxml==4.5.0",
-                      "python-dateutil==2.8.1",
-                      "requests==2.23.0",
-                      "six==1.14.0",
-                      "urllib3==1.25.8"]
+requirements = ["certifi==2019.11.28",
+                "chardet==3.0.4",
+                "idna==2.9",
+                "lxml==4.5.0",
+                "python-dateutil==2.8.1",
+                "requests==2.23.0",
+                "six==1.14.0",
+                "urllib3==1.25.8"]
 
-test_requirements = [ ]
+test_requirements = []
+setup_requirements = []
 
 setup(
     author="Shlomi Kushchi",
