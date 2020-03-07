fe27636305f9cdeff258f775063a0f43faac44e9
Shlomi Kushchi <shlomik@gmail.com>
2020-03-07 21:34:19 +0200

    create ability to stream data

--- simple_pastebin_parser/simple_pastebin_parser.py
+++ simple_pastebin_parser/simple_pastebin_parser.py
@@ -1,17 +1,36 @@
-from pprint import pprint
+from time import sleep
 
-from lxml import html
-import requests
-import dateutil.parser
+from simple_pastebin_parser.post import Post
+from simple_pastebin_parser.utils import parse_html, Url
 
 KNOWN_HREFS = ['/favicon.ico', '/i/pastebin.min.v9.css?1575389335', '/pro', '/api', '/faq', '/login', '/signup',
                '/archive', '/tools#chrome', '/languages', '/archive', '/faq', '/night_mode', '/dmca', '/contact']
 
 KNOWN_PREFIXES = ['http://', 'https://', '/tools', '/doc_', '/archive/']
+PASTEBIN_ARCHIVE_URL = 'https://pastebin.com/archive'
 
-def parse():
-    page = requests.get('https://pastebin.com/archive')
-    tree = html.fromstring(page.content)
+
+def get_posts(should_stream: bool=False, sampling_frequency=30):
+    """
+    get the current posts on pastebin.com as a snapshot.
+
+    :return:
+    """
+    hrefs = []
+    while 1:
+        tree = parse_html(Url(PASTEBIN_ARCHIVE_URL))
+        new_hrefs = get_all_hrefs_in_html(tree)
+
+        for href in new_hrefs:
+            if href not in hrefs:
+                yield Post(href)
+                hrefs.append(href)
+        sleep(sampling_frequency)
+        if not should_stream:
+            break
+
+
+def get_all_hrefs_in_html(tree):
     unfiltered_hrefs = [ref for ref in tree.xpath("//@href") if ref not in KNOWN_HREFS]
     hrefs = []
     for href in unfiltered_hrefs:
@@ -24,23 +43,5 @@ def parse():
             hrefs.append(href)
 
     hrefs = [href for href in hrefs if len(href) > 5]
+    return hrefs
 
-    for href in hrefs[:10]:
-        page = requests.get('https://pastebin.com{}'.format(href))
-        tree = html.fromstring(page.content)
-        # title = tree.xpath("//h1")[0].text
-        # title = tree.xpath("//div[@class='paste_box_line1']/h1")[0].text
-        title = tree.xpath("//div[contains(@class, 'paste_box_line1')]")[0].getchildren()[0].text
-        try:
-            author = tree.xpath("//div[contains(@class, 'paste_box_line2')]/a")[0].text
-        except:
-            author = "Guest"
-        date = dateutil.parser.parse(tree.xpath("//div[contains(@class, 'paste_box_line2')]/span")[0].text)
-        code = tree.xpath("//textarea[contains(@id, 'paste_code')]")[0].text
-        print("href: ", href)
-        print("Title: ", title)
-        print("Author: ", author)
-        print("date: ", date)
-        print(code)
-        print("*"*20)
-        print("*"*20)
