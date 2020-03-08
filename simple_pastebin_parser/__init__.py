74b0c0349653a5d0a7198d93b26bb38d9fef85a4
Shlomi Kushchi <shlomik@gmail.com>
2020-03-08 21:56:31 +0200

    add some logs, also wrap parsing with try: except: so one bad paring will not break entire execution

--- simple_pastebin_parser/__init__.py
+++ simple_pastebin_parser/__init__.py
@@ -3,10 +3,14 @@
 __author__ = """Shlomi Kushchi"""
 __email__ = 'shlomik@example.com'
 
+import logging
+
 from time import sleep
 
 from simple_pastebin_parser.paste import Paste
 from simple_pastebin_parser.utils import parse_html, Url
+
+logger = logging.getLogger("simple-pastebin-parser")
 
 KNOWN_HREFS = ['/favicon.ico', '/i/pastebin.min.v9.css?1575389335', '/pro', '/api', '/faq', '/login', '/signup',
                '/archive', '/tools#chrome', '/languages', '/archive', '/faq', '/night_mode', '/dmca', '/contact']
@@ -36,14 +40,21 @@ def get_pastes(should_stream: bool=False, sampling_frequency=60 * 2):
     :return: yields pastes
     """
     hrefs = []
+    if should_stream:
+        logger.info("start crawling pastebin.com with interval {}".format(sampling_frequency))
+    else:
+        logger.info("get pastebin.com pastes snapshot")
     while 1:
         tree = parse_html(Url(PASTEBIN_ARCHIVE_URL))
         new_hrefs = get_current_pastes_ids(tree)
 
         for href in new_hrefs:
-            if href not in hrefs:
-                yield Paste(href)
-                hrefs.append(href)
+            try:  # don't let parsing error break all other paste parsing
+                if href not in hrefs:
+                    yield Paste(href)
+                    hrefs.append(href)
+            except IndexError:
+                logger.exception("error while trying to parse paste: {}".format(href))
         trim_pastes_cache(hrefs)
         sleep(sampling_frequency)
         if not should_stream:
