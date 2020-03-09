9cc5288f63e888351c7b8c2b9b83d6480589941c
Shlomi Kushchi <shlomik@gmail.com>
2020-03-09 18:10:35 +0200

    v0.5.1

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/shlomikushchi/simple_pastebin_parser',
-    version='v0.5.0',
+    version='v0.5.1',
     zip_safe=False,
 )
